563bed722cce877c682d4de5dfc3e7ea3a213515
Neto Evtek <jsn.ads@gmail.com>
2023-10-27 17:16:27 -0300

    01 - introdução

---
+++ 01-Introdução_Geral/react.cmd
@@ -0,0 +1,13 @@
+COMO FUNCIONA O REACT NATIVE
+    Android -> Kotlin
+    iOS -> Swift
+
+    React NATIVE
+    - Cria o app em React NATIVE(que utliza React na base)
+    - Build (traduz o scrip na linguagem nativa do sistema)
+        - React Native
+
+FORMAS DE CRIAR UM PROJETO
+
+    - Expo CLI       (Iniciante)
+    - RN CLI         (Avançado)
