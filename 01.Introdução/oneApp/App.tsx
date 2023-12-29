c9f2f07829fd3ed5ed30dbd95441be30c843ac44
Neto Evtek <jsn.ads@gmail.com>
2023-12-29 17:00:53 -0300

    01 - Corrigindo Estrutura

--- 01.Introdução/oneApp/App.tsx
+++ 01.Introdução/oneApp/App.tsx
@@ -4,7 +4,7 @@ import { StyleSheet, Text, View } from 'react-native';
 export default function App() {
   return (
     <View style={styles.container}>
-      <Text>Meu Primeiro App</Text>
+      <Text>Primeiro App</Text>
       <StatusBar style="auto" />
     </View>
   );
@@ -13,7 +13,7 @@ export default function App() {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#CCC',
+    backgroundColor: '#fff',
     alignItems: 'center',
     justifyContent: 'center',
   },
