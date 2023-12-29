3172c48fcabd45c1b6a0bdf410ac09139550312b
Neto Evtek <jsn.ads@gmail.com>
2023-12-29 17:40:56 -0300

    02.1 - Entendendo Componentes

---
+++ 02.Fundamentos de  React para RN/01.Entendendo Componentes/twoOneApp/App.tsx
@@ -0,0 +1,12 @@
+import { Text, View } from "react-native";
+
+function twoOneApp()
+{
+  return(
+    <View>
+      <Text>twoOneApp</Text>
+    </View>
+  )
+}
+
+export default twoOneApp
---
+++ 02.Fundamentos de  React para RN/02.Criando o proprio componente/twoTwoApp/App.tsx
@@ -0,0 +1,12 @@
+import { Text, View } from "react-native";
+
+function twoOneApp()
+{
+  return(
+    <View>
+      <Text>twoTwoApp</Text>
+    </View>
+  )
+}
+
+export default twoOneApp
